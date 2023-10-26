00af547ed49480c1967b9be9ed29af723cdb435c
joeCodes05 <nathancodes05@gmail.com>
2023-10-26 15:03:11 +0100

    Large screen UI change

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,41 +1,70 @@
 import React from 'react'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { ContextProvider } from '../App'
+import Button from '../components/button'
+import InputFields from '../components/inputFields'
 
 const SignUp = () => {
+  const theme = useContext(ContextProvider);
   return (
     <>
-        <div className='h-screen w-screen flex items-center justify-center'>
-            <div className='max-w-lg'>
-                <h1 className='text-accent text-center font-rubik text-[50px] leading-[50px] font-bold'>
-                    Geekers
-                </h1>
-                <h5 className='text-gray-800 text-center font-poppins'>
-                    Connect With Your Kind
-                </h5>
-
-                <form action="#" className='mt-4 flex gap-y-4 flex-col' method='post'>
-                    <div className='username_input'>
-                        <label htmlFor="username" className='text-gray-800 font-semibold'>Username</label>
-                        <input type="text" className='w-[25rem] block p-1.5 outline-none ring-1 ring-gray-300 rounded-md focus:ring-2 focus:ring-accent'/>
-                    </div>
-                    <div className='email_input'>
-                        <label htmlFor="email" className='text-gray-800 font-semibold'>Email address</label>
-                        <input type="email" className='w-[25rem] block p-1.5 outline-none ring-1 ring-gray-300 rounded-md focus:ring-2 focus:ring-accent'/>
-                    </div>
-                    <div className='password_input'>
-                        <label htmlFor="password" className='text-gray-800 font-semibold'>Password</label>
-                        <input type="password" className='w-[25rem] block p-1.5 outline-none ring-1 ring-gray-300 rounded-md focus:ring-2 focus:ring-accent'/>
-                    </div>
-                    <div className='link_el'>
-                        <div className="text-gray-500">
-                            Already have an account? <Link to={'/signin'} className='text-accent underline'>Login</Link>
-                        </div>
-                    </div>
-                    <div className="submit_btn">
-                        <button className='w-full p-1.5 rounded-md font-rubik bg-accent ring-2 ring-accent text-primary'>Signin</button>
-                    </div>
-                </form>
+        <div className='h-screen w-screen flex items-center'>
+          <div className='w-full'>
+            <h1 className='text-center text-[25px] leading-[25px] md:text-[35px] md:leading-[35px] ld:text-[40px] leading-[40px] text-emerald-700 font-bold font-rubik'>
+              Create Your Account Today!
+            </h1>
+            <div className="text-gray-500 mt-4 text-sm md:w-[50%] mx-auto text-center">
+              Unlock the Ultimate Geek Experience on Geekers!
+              <br />
+               join Geekers for Geeky fun.
             </div>
+
+            <div className="mt-10 mx-auto w-[700px]">
+              <div className="flex">
+                <div className='md:border-r md:border-r-gray-300 w-[50%] p-5'>
+                  <form action="#" method='post' className='flex flex-col gap-y-4'>
+                    <div>
+                      <InputFields 
+                        inputType={'email'}
+                        placeholder={'example@email.com'}
+                      />
+                    </div>
+                    <div>
+                      <InputFields 
+                        inputType={'password'}
+                        placeholder={'Password'}
+                      />
+                    </div>
+                    <div>
+                      <Button 
+                        buttonText={'Create account'}
+                        outline={false}
+                        width={'w-full'}
+                      />
+                    </div>
+                  </form>
+                </div>
+
+                <div className='p-5 w-[50%]'>
+                  <div className="flex flex-col gap-4">
+                    <Button 
+                      buttonText={'Sign in with Google'}
+                      outline={true}
+                    />
+                    <Button 
+                      buttonText={'Sign in with Facebook'}
+                      outline={true}
+                    />
+                    <Button 
+                      buttonText={'Sign in with Apple'}
+                      outline={true}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
     </>
   )
