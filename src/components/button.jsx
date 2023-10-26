8fee090dd8fe2441816aca11c2add4e2b65264e8
joeCodes05 <nathancodes05@gmail.com>
2023-10-26 18:33:58 +0100

    code updates to UI components

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({ buttonText, outline, width }) => {
   return (
     <>
-        <button className={outline ? `py-2 px-5 shadow-lg rounded-md outline-none ring-2 ring-emerald-700 bg-transparent text-emerald-700 ${width}` : `py-2 px-5 shadow-lg rounded-md ring-2 ring-emerald-700 bg-emerald-700 duration-700 hover:bg-emerald-800 hover:ring-emerald-800 text-white ${width}`}>
+        <button className={outline ? `py-2 px-5 shadow-md rounded-md outline-none ring-2 ring-emerald-700 bg-transparent text-emerald-700 ${width}` : `py-2 px-5 shadow-md rounded-md ring-2 ring-emerald-700 bg-emerald-700 duration-700 hover:bg-emerald-800 hover:ring-emerald-800 text-white ${width}`}>
             {buttonText}
         </button>
     </>
--- src/components/inputFields.jsx
+++ src/components/inputFields.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const InputFields = ({ inputType, placeholder }) => {
   return (
     <>
-        <input type={inputType} placeholder={placeholder} className='block w-full p-2 shadow-lg ring-2 ring-gray-300 rounded-md duration-700 focus:ring-emerald-700 outline-none' />
+        <input type={inputType} placeholder={placeholder} className='block w-full p-2 shadow-md ring-1 ring-gray-300 rounded-md duration-700 focus:ring-emerald-700 outline-none' />
     </>
   )
 }
