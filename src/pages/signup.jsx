a69ae953562d293786e6e71653070d43e957917e
joeCodes05 <nathancodes05@gmail.com>
2023-10-26 18:34:37 +0100

    navbar form completed and fully responsive

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { ContextProvider } from '../App'
 import Button from '../components/button'
 import InputFields from '../components/inputFields'
+import * as BsIcons from 'react-icons/bs'
 
 const SignUp = () => {
   const theme = useContext(ContextProvider);
@@ -11,7 +12,7 @@ const SignUp = () => {
     <>
         <div className='h-screen w-screen flex items-center'>
           <div className='w-full'>
-            <h1 className='text-center text-[25px] leading-[25px] md:text-[35px] md:leading-[35px] ld:text-[40px] leading-[40px] text-emerald-700 font-bold font-rubik'>
+            <h1 className='text-center text-[25px] leading-[25px] md:text-[35px] md:leading-[35px] ld:text-[40px] lg:leading-[40px] text-emerald-700 font-bold font-rubik'>
               Create Your Account Today!
             </h1>
             <div className="text-gray-500 mt-4 text-sm md:w-[50%] mx-auto text-center">
@@ -20,49 +21,50 @@ const SignUp = () => {
                join Geekers for Geeky fun.
             </div>
 
-            <div className="mt-10 mx-auto w-[700px]">
-              <div className="flex">
-                <div className='md:border-r md:border-r-gray-300 w-[50%] p-5'>
-                  <form action="#" method='post' className='flex flex-col gap-y-4'>
-                    <div>
-                      <InputFields 
-                        inputType={'email'}
-                        placeholder={'example@email.com'}
-                      />
-                    </div>
-                    <div>
-                      <InputFields 
-                        inputType={'password'}
-                        placeholder={'Password'}
-                      />
-                    </div>
-                    <div>
-                      <Button 
-                        buttonText={'Create account'}
-                        outline={false}
-                        width={'w-full'}
-                      />
-                    </div>
-                  </form>
-                </div>
-
-                <div className='p-5 w-[50%]'>
-                  <div className="flex flex-col gap-4">
-                    <Button 
-                      buttonText={'Sign in with Google'}
-                      outline={true}
-                    />
-                    <Button 
-                      buttonText={'Sign in with Facebook'}
-                      outline={true}
-                    />
-                    <Button 
-                      buttonText={'Sign in with Apple'}
-                      outline={true}
+            <div className="mt-5 mx-auto px-5 md:px-0 w-full md:w-[450px]">
+              <form action="#" method='post' className='flex flex-col gap-y-4'>
+                  <div>
+                    <InputFields 
+                      inputType={'text'}
+                      placeholder={'Full name'}
                     />
                   </div>
+                  <div>
+                    <InputFields 
+                      inputType={'email'}
+                      placeholder={'example@email.com'}
+                    />
+                  </div>
+                  <div>
+                    <InputFields 
+                      inputType={'password'}
+                      placeholder={'Password'}
+                    />
+                  </div>
+                  <div>
+                    <Button 
+                      buttonText={'Create account'}
+                      outline={false}
+                      width={'w-full'}
+                    />
+                  </div>
+                </form>
+                <div className="mt-5 text-center text-gray-500">
+                  Or sign up with
                 </div>
-              </div>
+                <div className="mt-5">
+                  <div className="md:flex grid grid-cols-2 gap-4">
+                    <button className='py-3 px-5 bg-[#ea4335] ring-2 ring-[#ea4335] w-full text-white flex items-center gap-3 rounded-md shadow-md'>
+                        <BsIcons.BsGoogle /> Google
+                    </button>
+                    <button className='py-3 px-5 bg-[#3b5998] ring-2 ring-[#3b5998] w-full text-white flex items-center gap-3 rounded-md shadow-md'>
+                        <BsIcons.BsFacebook /> Facebook
+                    </button>
+                    <button className='py-3 px-5 bg-[#0072b1] ring-2 ring-[#0072b1 ] w-full text-white flex items-center gap-3 rounded-md shadow-md'>
+                        <BsIcons.BsLinkedin /> Linkedin
+                    </button>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
