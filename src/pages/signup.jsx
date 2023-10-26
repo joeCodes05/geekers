af71a81e404a3231bba5523217433ac65095f0d0
joeCodes05 <nathancodes05@gmail.com>
2023-10-26 07:02:37 +0100

    added input fields and button

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,79 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
     <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
+        <div className='h-screen w-screen flex items-center justify-center'>
+            <div className='max-w-lg'>
+                <h1 className='text-accent text-center font-rubik text-[50px] leading-[50px] font-bold'>
+                    Geekers
+                </h1>
+                <h5 className='text-gray-800 text-center font-poppins'>
+                    Connect With Your Kind
+                </h5>
+
+                <form action="#" className='mt-4 flex gap-y-4 flex-col' method='post'>
+                    <div className='username_input'>
+                        <label htmlFor="username" className='text-gray-800 font-semibold'>Username</label>
+                        <input type="text" className='w-[25rem] block p-1.5 outline-none ring-1 ring-gray-300 rounded-md focus:ring-2 focus:ring-accent'/>
+                    </div>
+                    <div className='email_input'>
+                        <label htmlFor="email" className='text-gray-800 font-semibold'>Email address</label>
+                        <input type="email" className='w-[25rem] block p-1.5 outline-none ring-1 ring-gray-300 rounded-md focus:ring-2 focus:ring-accent'/>
+                    </div>
+                    <div className='password_input'>
+                        <label htmlFor="password" className='text-gray-800 font-semibold'>Password</label>
+                        <input type="password" className='w-[25rem] block p-1.5 outline-none ring-1 ring-gray-300 rounded-md focus:ring-2 focus:ring-accent'/>
+                    </div>
+                    <div className='link_el'>
+                        <div className="text-gray-500">
+                            Already have an account? <Link to={'/signin'} className='text-accent underline'>Login</Link>
+                        </div>
+                    </div>
+                    <div className="submit_btn">
+                        <button className='w-full p-1.5 rounded-md font-rubik bg-accent ring-2 ring-accent text-primary'>Signin</button>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
-            </a>
-          </p>
-        </div>
-      </div>
     </>
   )
 }
