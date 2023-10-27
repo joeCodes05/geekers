b53ae86dcf055a059442c409911cf55eb854c16e
joeCodes05 <nathancodes05@gmail.com>
2023-10-27 21:38:41 +0100

    worked on hero section

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import heroImg from '../assets/images/hero_img.png'
+import { Link } from 'react-router-dom'
+import Button from '../components/button'
 
 const Homepage = () => {
   return (
-    <div>homepage</div>
+    <>
+      <main className='homepage'>
+        <section className='hero_section'>
+          <div className='h-full py-[7rem] w-screen place-items-center grid text-center'>
+            <div>
+              <h1 className='text-emerald-700 text-2xl leading-[24px] md:text-5xl md:leading-[48px] font-rubik capitalize'>Unleash your inner <br /> superhero in the digital age.</h1>
+              <p className="mt-3 text-gray-500 w-full md:w-[70%] lg:w-[55%] mx-auto text-sm">
+                In a tech-driven world, Geekers are the unsung heroes, wielding knowledge, creativity, and passion. Embrace your inner superhero and let's change the world one code at a time!"
+              </p>
+              <div className="mt-6">
+                <div className="flex gap-4 mx-auto w-fit">
+                  <Link to={'/articles'} className='no-underline w-fit'>
+                    <Button 
+                      buttonText={'Read Articles'}
+                      outline={true}
+                    />
+                  </Link>
+                  <Link to={'/community'} className='no-underline w-fit'>
+                    <Button 
+                      buttonText={'Join Community'}
+                      outline={false}
+                    />
+                  </Link>
+                </div>
+              </div>
+              <div className="mt-6">
+                <img src={heroImg} className='mx-auto' width={'400'} alt="laptop code mockup" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
