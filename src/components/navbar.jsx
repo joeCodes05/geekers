5af054936f91f465771ff2da08c5bc4d6a0aa550
joeCodes05 <nathancodes05@gmail.com>
2023-10-27 21:38:14 +0100

    code update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,11 +8,10 @@ import { Link } from 'react-router-dom'
 import Button from './button'
 
 const navLinks = [
-    {linkName: "Home", path: "/", isActive: false},
-    {linkName: "Courses", path: "/courses", isActive: false},
-    {linkName: "Articles", path: "/articles", isActive: false},
-    {linkName: "Community", path: "/community", isActive: false},
-    {linkName: "FAQs", path: "/faq", isActive: false}
+  {linkName: "Home", path: "/"},
+  {linkName: "Courses", path: "/courses"},
+  {linkName: "Articles", path: "/articles"},
+  {linkName: "FAQs", path: "/faq"}
 ]
 
 export default function Navbar() {
@@ -37,9 +36,9 @@ export default function Navbar() {
 
   return (
     <header className={navbar ? "bg-white fixed w-full top-0 left-0 right-0 shadow-md duration-700" : "bg-white fixed w-full top-0 left-0 right-0 duration-700"}>
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-3 md:px-0 md:py-3" aria-label="Global">
+      <nav className="mx-auto flex w-[95%] items-center justify-between p-3 md:px-0 md:py-3" aria-label="Global">
         <div className="flex lg:flex-1">
-          <Link to={'/'} className='text-3xl font-rubik font-bold text-emerald-700'>Geekers</Link>
+          <Link to={'/'} className='text-3xl font-rubik text-emerald-700'>Geekers</Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -53,7 +52,7 @@ export default function Navbar() {
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-8">
             {navLinks.map((data, index) => {
-                const { linkName, path, isActive } = data;
+                const { linkName, path } = data;
                 return (
                     <Link to={path} key={index} className='font-rubik text-md duration-700 text-gray-800 hover:text-emerald-700'>
                         {linkName}
@@ -62,6 +61,13 @@ export default function Navbar() {
             })}            
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:gap-3 lg:justify-end">
+            <Link to={'/login'} className='no-underline sm:inline-block md:hidden xl:inline-block'>
+              <Button
+                buttonText={'Log in'}
+                outline={true}
+              />
+            </Link>
+
             <Link to={'/signup'} className='no-underline'>
                 <Button
                     buttonText={'Sign up'}
@@ -74,7 +80,7 @@ export default function Navbar() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link to={'/'} className='text-3xl font-rubik font-bold text-emerald-700'>Geekers</Link>
+            <Link to={'/'} className='text-3xl font-rubik text-emerald-700'>Geekers</Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -88,9 +94,9 @@ export default function Navbar() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navLinks.map((data, index) => {
-                    const { linkName, path, isActive } = data;
+                    const { linkName, path } = data;
                     return (
-                        <Link to={path} key={index} className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'>
+                        <Link to={path} key={index} className='-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-gray-900 hover:bg-emerald-50'>
                             {linkName}
                         </Link>
                     )
